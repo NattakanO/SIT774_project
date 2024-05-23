186e3ee816ae11cd2949d0010f1ec3d1b4be076d
Nattakan <nattakan.o1998@gmail.com>
2024-05-23 11:13:25 +1000

    add gitignore

--- createDB.js
+++ createDB.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('myDB');
+const db = new sqlite3.Database('projectDB.db');
 
 db.serialize(() => {
   db.run("CREATE TABLE IF NOT EXISTS feedback (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, icecreamtype TEXT, rating INT, feedback TEXT)"); 
